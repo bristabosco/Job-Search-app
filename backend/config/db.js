2824bdcadd667abda921d4fbffae624fbd39a488
crow00721 <prathishselvam123@gmail.com>
2025-04-14 14:14:50 +0530

    updated code

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/JobSearch");
+    await mongoose.connect("mongodb+srv://brista:bristamery@cluster0.26yem.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
     console.log('MongoDB Connected');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
